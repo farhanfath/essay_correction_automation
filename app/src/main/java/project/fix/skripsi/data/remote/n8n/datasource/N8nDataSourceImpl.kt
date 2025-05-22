175e49646f000dfc436bd0701efce026799cf3c9
farhan <farhanfathur73@gmail.com>
2025-05-22 16:06:07 +0700

    feat: wrap request handler to api

--- app/src/main/java/project/fix/skripsi/data/remote/n8n/datasource/N8nDataSourceImpl.kt
+++ app/src/main/java/project/fix/skripsi/data/remote/n8n/datasource/N8nDataSourceImpl.kt
@@ -1,15 +1,11 @@
 package project.fix.skripsi.data.remote.n8n.datasource
 
-import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okhttp3.Headers
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import project.fix.skripsi.data.remote.n8n.N8nApiService
 import project.fix.skripsi.data.remote.n8n.model.WebhookResponse
+import project.fix.skripsi.data.utils.createJsonPart
+import project.fix.skripsi.data.utils.toImagePart
 import java.io.File
 import javax.inject.Inject
 
@@ -25,32 +21,21 @@ class N8nDataSourceImpl @Inject constructor(
     ): Result<WebhookResponse> {
         return withContext(Dispatchers.IO) {
             try {
-                // image data
-                val requestFile = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
-                val imagePart = MultipartBody.Part.createFormData("image", imageFile.name, requestFile)
+                val imagePart = imageFile.toImagePart("image")
 
-                // essayType as JSON string
-                val quizCategoryJsonString = """{"source":"android","quizType":"$quizType"}"""
-                val quizCategoryRequestBody = quizCategoryJsonString.toRequestBody("text/plain".toMediaTypeOrNull())
-                val quizTypePart = MultipartBody.Part.create(
-                    Headers.headersOf("Content-Disposition", "form-data; name=\"type\""),
-                    quizCategoryRequestBody
+                val quizTypePart = createJsonPart(
+                    partName = "type",
+                    content = mapOf("source" to "android", "quizType" to quizType)
                 )
 
-                // evaluation_category
-                val evaluationCategoryJsonString = """{"source":"android","evaluationCategory":"$evaluationCategory"}"""
-                val evaluationCategoryRequestBody = evaluationCategoryJsonString.toRequestBody("text/plain".toMediaTypeOrNull())
-                val evaluationCategoryPart = MultipartBody.Part.create(
-                    Headers.headersOf("Content-Disposition", "form-data; name=\"evaluation_category\""),
-                    evaluationCategoryRequestBody
+                val evaluationCategoryPart = createJsonPart(
+                    partName = "evaluation_category",
+                    content = mapOf("source" to "android", "evaluationCategory" to evaluationCategory)
                 )
 
-                // answer_key
-                val answerKeyJson = Gson().toJson(answerKey)
-                val answerKeyBody = answerKeyJson.toRequestBody("text/plain".toMediaTypeOrNull())
-                val answerKeyPart = MultipartBody.Part.create(
-                    Headers.headersOf("Content-Disposition", "form-data; name=\"answer_key\""),
-                    answerKeyBody
+                val answerKeyPart = createJsonPart(
+                    partName = "answer_key",
+                    content = answerKey
                 )
 
                 val response = n8nApiService.evaluateEssay(imagePart, quizTypePart, evaluationCategoryPart, answerKeyPart)
---
+++ app/src/main/java/project/fix/skripsi/data/utils/RequestExtension.kt
@@ -0,0 +1,23 @@
+package project.fix.skripsi.data.utils
+
+import com.google.gson.Gson
+import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+
+fun File.toImagePart(partName: String): MultipartBody.Part {
+  val requestFile = asRequestBody("image/*".toMediaTypeOrNull())
+  return MultipartBody.Part.createFormData(partName, name, requestFile)
+}
+
+fun createJsonPart(partName: String, content: Any): MultipartBody.Part {
+  val json = Gson().toJson(content)
+  val requestBody = json.toRequestBody("text/plain".toMediaTypeOrNull())
+  return MultipartBody.Part.create(
+    Headers.headersOf("Content-Disposition", "form-data; name=\"$partName\""),
+    requestBody
+  )
+}
